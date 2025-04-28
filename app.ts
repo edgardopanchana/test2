857dbd98b56ec9bcbd1eaa902a6914dbd2ad9f8a
Edgardo Panchana <edgardopanchana@Edgardos-MacBook-Pro.local>
2025-04-28 16:27:32 -0500

    primer mensaje

---
+++ app.ts
@@ -0,0 +1,5 @@
+/*
+Vamos a modelar una clase Persona que tenga propiedades
+ básicas como nombre, edad y correo electrónico. Además, 
+ tendrá métodos para saludar, actualizar el correo 
+ y mostrar un resumen de la persona.*/
