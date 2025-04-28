a5427394d5b5932b51da33b49e20d97d39d2c7d5
Edgardo Panchana <edgardopanchana@Edgardos-MacBook-Pro.local>
2025-04-28 16:34:51 -0500

    texto modificado

--- app.ts
+++ app.ts
@@ -3,3 +3,5 @@ Vamos a modelar una clase Persona que tenga propiedades
  básicas como nombre, edad y correo electrónico. Además, 
  tendrá métodos para saludar, actualizar el correo 
  y mostrar un resumen de la persona.*/
+
+ /* esto es una modificacion*/
